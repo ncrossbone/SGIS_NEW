8b27e95a5b3b2c8b693c7e06995db5d74c79f283
pdj <ncrossbone@gmail.com>
2017-10-16 13:02:37 +0900

    외부망 - 임시 커밋

--- app.js
+++ app.js
@@ -3,6 +3,54 @@
  * needed for your application, but these edits will have to be merged by
  * Sencha Cmd when upgrading.
  */
+
+var _API = null;
+
+var store = Ext.create('Ext.data.Store', {
+	autoLoad : true,
+
+	fields : [ {
+		name : 'MapserviceUrl'
+	} ],
+
+	proxy : {
+		type : 'ajax',
+		url : './resources/data/LayerMapper.json',
+		reader : {
+			type : 'json'
+		}
+	},
+
+	listeners : {
+		beforeload : function(a, b, c) {
+			// //console.info(this.data.record);
+			_testUrl = "sss";
+		}
+	}
+});
+
+store.load(function(a, b, c) {
+	
+	_API = a[0].data;
+	
+});
+
+var proxy = './proxy/proxy.jsp?';
+	
+//지번 레이어 레전드 json 전역변수 설정
+var _LCLlegend= null;
+Ext.Ajax.request({
+	url: proxy+"http://112.217.167.123:23002/arcgis/rest/services/LSMD_CONT_LDREG/MapServer/legend?f=pjson",
+	async: true, 
+	success : function(response, opts) {
+		jsonData = Ext.util.JSON.decode( response.responseText );
+		_LCLlegend = jsonData.layers;
+	},
+	failure: function(form, action) {
+		alert("오류가 발생하였습니다.");
+	}
+});
+
 Ext.application({
     name: 'Sgis',
 
--- app/store/Area2Store.js
+++ app/store/Area2Store.js
@@ -11,8 +11,7 @@ Ext.define('Sgis.store.Area2Store', {
 	listeners: {
 		beforeload: function(store) {
 			Ext.defer(function() {
-				//var queryTask = new esri.tasks.QueryTask("http://cetech.iptime.org:6080/arcgis/rest/services/Layer2/MapServer/23"); //시군구
-				var queryTask = new esri.tasks.QueryTask("http://112.218.1.243:20002/arcgis/rest/services/Layer2_new/MapServer/16"); //시군구
+				var queryTask = new esri.tasks.QueryTask(_API.layer2_new +"/"+_API.admSggLayerId); //시군구
 				var query = new esri.tasks.Query();
 				query.returnGeometry = false;
 				query.where = "1=1";
